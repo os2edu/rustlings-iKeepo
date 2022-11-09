4f4a83b27d537680ae3e3920e82318d786f8a5d1
ikeepo <jizhijixing@163.com>
2022-11-09 23:02:14 +0800

    67

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -14,7 +14,6 @@
 
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub struct ReportCard {
     pub grade: f32,
@@ -24,8 +23,14 @@ pub struct ReportCard {
 
 impl ReportCard {
     pub fn print(&self) -> String {
+        if &self.student_age < &12 {
+        format!("{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, "A+")
+        } else {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
+
+        }
     }
 }
 
